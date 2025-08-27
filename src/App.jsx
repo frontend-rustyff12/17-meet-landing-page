1b914fdc38a85fc3fca611ebc4229fc1d1af5b12
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-27 16:00:10 +0200

    path fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Header from "components/Header";
-import Hero from "components/Hero";
-import MainContent from "components/MainContent";
-import Footer from "components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
