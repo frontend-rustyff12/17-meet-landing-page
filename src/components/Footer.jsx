c978f2eaf676535e28c6b096de36bd82e30efc6d
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-27 18:32:21 +0200

    Footer mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
 export default function Footer() {
   return (
-    <>
-      <h2>Footer goes here</h2>
-    </>
+    <footer>
+      <span className="number-circle red-hat-5">02</span>
+      <div className="hero">
+        <div className="content">
+          <h2 className="red-hat-2">Experience more together</h2>
+          <p className="red-hat-4">
+            Stay connected with reliable HD meetings and unlimited one-on-one
+            and group video sessions.
+          </p>
+
+          <button className="red-hat-5">Download v1.3</button>
+        </div>
+      </div>
+    </footer>
   );
 }
