1ac9e373a50548beacc6e3acf2af57b1e2f8a4ad
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-28 12:14:36 +0200

    README.md Update.Small CSS refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import MainContent from "./components/MainContent";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <Header />
-      <Hero />
-      <MainContent />
-      {/* <Footer /> */}
+      <main className="main-wrapper">
+        <Hero />
+        <MainContent />
+      </main>
+      <Footer />
     </>
   );
 }
