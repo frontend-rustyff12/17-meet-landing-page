1bdfc86c62aaa1689226a22ac8a660765dd443d9
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-28 10:44:52 +0200

    Mobile Main Content

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,35 @@
 export default function MainContent() {
+  const images = [
+    "woman-in-videocall",
+    "women-videochatting",
+    "men-in-meeting",
+    "man-texting",
+  ];
   return (
-    <>
-      <h2>MainContent Goes here</h2>
-    </>
+    <section className="main-content">
+      <span className="number-circle">01</span>
+      <div className="image-container">
+        {images.map((item, index) => (
+          <img
+            className=""
+            key={index}
+            src={`/desktop/image-${item}.jpg`}
+            // src="/desktop/image-man-texting.jpg"
+            alt={item.split("-").join("")}
+          />
+        ))}
+      </div>
+      <div className="text-container">
+        <h3 className="red-hat-3">Built for modern use</h3>
+
+        <h2 className="red-hat-2">Smarter meetings, all in one place</h2>
+
+        <p className="">
+          Send messages, share files, show your screen, and record your meetings
+          — all in one workspace. Control who can join with invite-only team
+          access, data encryption, and data export.
+        </p>
+      </div>
+    </section>
   );
 }
