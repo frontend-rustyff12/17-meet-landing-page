6fec28d61226e2ba9290b0bdc8a6763476133dcb
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-28 07:01:22 +0200

    Hero mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <Header />
       <Hero />
       <MainContent />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
